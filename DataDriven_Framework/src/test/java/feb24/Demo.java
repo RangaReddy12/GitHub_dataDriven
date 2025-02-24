11a4ec1661857bc185b85202f627046fba9fe71e
Qedge <Qedge@DESKTOP-1RLTEAV.Dlink>
2025-02-24 12:52:49 +0530

    New Class added

---
+++ DataDriven_Framework/src/test/java/feb24/Demo.java
@@ -0,0 +1,16 @@
+package feb24;
+
+public class Demo {
+public void add()
+{
+	System.out.println("Iam happy");
+}
+public void div()
+{
+	System.out.println("Iam sad");
+}
+public void mul()
+{
+	System.out.println("Iam lucky");
+}
+}
